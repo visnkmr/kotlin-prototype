8c8d1b3819629da966f55f02757bef4b2988ddaf
12core1 <12533753+visnkmr@users.noreply.github.com>
2025-09-05 20:27:58 +0530

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 plugins {
-    kotlin("jvm") version "1.9.21"
+    java
     application
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.example"
@@ -11,23 +14,27 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(17)
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 application {
-    mainClass.set("com.example.MainKt")
+    mainClass.set("com.example.MainApplication")
 }
 
 tasks.jar {
     manifest {
-        attributes["Main-Class"] = "com.example.MainKt"
+        attributes["Main-Class"] = "com.example.MainApplication"
     }
 }
